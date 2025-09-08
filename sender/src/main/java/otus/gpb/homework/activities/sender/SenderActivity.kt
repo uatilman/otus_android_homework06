7f1851a77df90e51049b2a29a3a85c9817005f67
YURY TILMAN <uatilman@gmail.com>
2025-09-08 22:33:22 +0300

    Create SenderActivity, buttons and it listeners

---
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -0,0 +1,33 @@
+package otus.gpb.homework.activities.sender
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import otus.gpb.homework.activities.receiver.R
+
+class SenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sender)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.to_google_maps_button).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.send_email_button).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.open_receiver_button).setOnClickListener {
+
+        }
+
+    }
+}
